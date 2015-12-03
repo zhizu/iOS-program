fbad76090bf6ae7bb1cddb7652784042582e10a3
zhizu <xuxu_1111@126.com>
2015-12-03 10:34:20 +0900

    update xcode project

---
+++ NewsDemo/NewsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsDemo
+//
+//  Created by 楊旭 on 2015/12/03.
+//  Copyright © 2015年 楊旭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
