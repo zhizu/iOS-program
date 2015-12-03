ad6b405e59ff2c361f366d4ce139c78c2e32eabc
asahi <yang.asahi@gmail.com>
2015-12-03 09:28:19 +0900

    update python

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import urllib2
+import json,httplib
 from bs4 import BeautifulSoup
 #print "Hello World!"
 
@@ -19,6 +20,21 @@ for s in soup.find('ul', class_="list").find_all('li'):
        print s.findAll('img')[0]['data-src']
     else:
        print "null"
+    connection = httplib.HTTPSConnection('api.parse.com', 443)
+    connection.connect()
+    connection.request('POST', '/1/classes/YahooNews', json.dumps({
+       "title": s.find("span","ttl").string,
+       "type": s.find("span","cate").string,
+       "url": s.find('a', href=True)['href'],
+       "newsdate": s.find("span","date").string
+     }), {
+       "X-Parse-Application-Id": "Pz7iwzIf7ocSS356iONXA8kiIxrUa9rteGIDbOp5",
+       "X-Parse-REST-API-Key": "7aEJ3ogfoClcuFVU2Td66i6HWG5aC7ie9T4y2RpG",
+       "Content-Type": "application/json"
+     })
+    results = json.loads(connection.getresponse().read())
+    print results
+
 
 
 
